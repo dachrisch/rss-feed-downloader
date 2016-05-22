a1c4b614a241b96cfd90988443d78bab3cc3fe4f
Christian Dähn <christian.daehn@gmail.com>
2016-05-22 13:30:32 +0200

    new test to remove parameters from url

--- tests/rss_feed_downloader_test.py
+++ tests/rss_feed_downloader_test.py
@@ -52,6 +52,17 @@ class VodcastFeedDownloaderTest(unittest.TestCase):
         self.assertEqual(vodcast.local_filename, 'TV-20101023-2220-5801.h264.mp4')
         self.assertEqual(vodcast.updated, datetime(2010, 10, 26, 10, 53, 49))
 
+    def test_givenVideoUrlWithParametersWhenGeneratingLocalFileThenParametersAreStriped(self):
+        entry = type('Entry', (object,), {}) 
+        entry.title = 'test_givenVideoUrlWithParametersWhenGeneratingLocalFileThenParametersAreStriped'
+        entry.enclosures = (type('Entry', (object,), {})  , )
+        entry.enclosures[0].type = 'video/mp4'
+        entry.enclosures[0].href = 'TV-20101023-2220-5801.h264.mp4?should_be_removed'
+        entry.updated_parsed = (2010, 10, 26, 10, 53, 49, 0, 0, 0)
+        vodcast = parse_video_item(entry)
+
+        self.assertEqual(vodcast.local_filename, 'TV-20101023-2220-5801.h264.mp4')
+
     def test_givenUrlPointingToLocalResourceWhenDownloadedThenContentWillBeStored(self):
         entries = self.rss_feed.entries
         vodcast = parse_video_item(entries[0])
@@ -150,23 +161,6 @@ class VodcastFeedDownloaderTest(unittest.TestCase):
     def test_create_md5_hash(self):
         import hashlib
         self.assertEqual( hashlib.sha224("http://some.domain/rss.xml").hexdigest(), '28dbff3e80675af61e810c911d19ce690d2497a03f64d47f5559199e')
-
-    def test_stripParametersFromUrl(self):
-        items = feedparser.parser('''<?xml version='1.0' encoding='UTF-8'?>
-                            <rss version='2.0'>
-                            <channel>
-                            <title>Extra3</title>
-                            <item>
-                                <title>Extra 3 three</title>
-                                <description>Tatort Taliban; Jasmin trifft: Frau zu Guttenberg</description>
-                                <pubDate>Tue, 28 Oct 2010 11:53:49 +0200</pubDate>
-                                <enclosure url='http://media.ndr.de/download/podcasts/extradrei196/TV-20101028-2220-5801.h264.mp4' type='video/mp4' />
-                                <guid isPermaLink='false'>TV-20101028-2220-5801-V</guid>
-                                <link>http://download.ted.com/talks/AntonyGormley_2012G-480p.mp4?apikey=TEDRSS</link>
-                            </item>
-                            </channel></rss>''')
-        v = Vodcast(items[0])
-        self.assertEqual('AntonyGormley_2012G-480p.mp4', v.local_filename)
 
 if __name__ == '__main__':
     import logging
